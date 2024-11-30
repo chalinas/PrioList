8bc1afdd7dbbe6f2789bdcaae96c33b343965711
Chalinas <albertogomezaranda@gmail.com>
2024-11-30 19:08:02 +0100

    Layouts and icons

--- PrioList/app/build.gradle.kts
+++ PrioList/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.chalinas.priolist"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.chalinas.priolist"
@@ -32,6 +32,10 @@ android {
     }
     kotlinOptions {
         jvmTarget = "11"
+    }
+
+    buildFeatures {
+        viewBinding = true
     }
 }
 
--- PrioList/app/src/main/java/com/chalinas/priolist/MainActivity.kt
+++ PrioList/app/src/main/java/com/chalinas/priolist/MainActivity.kt
@@ -1,20 +1,45 @@
 package com.chalinas.priolist
 
+import android.app.Dialog
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.chalinas.priolist.databinding.ActivityMainBinding
+import com.chalinas.priolist.utils.setupDialog
 
 class MainActivity : AppCompatActivity() {
+
+    private val mainBinding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    private val addTaskDialog: Dialog by lazy {
+        Dialog(this).apply{
+            setupDialog(R.layout.add_task_dialog)
+        }
+    }
+
+    private val updateTaskDialog: Dialog by lazy {
+        Dialog(this).apply{
+            setupDialog(R.layout.update_task_dialog)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(mainBinding.root)
+
+        val addCloseImg = addTaskDialog.findViewById<ImageView>(R.id.closeImg)
+        val updateCloseImg = updateTaskDialog.findViewById<ImageView>(R.id.closeImg)
+
+        addCloseImg.setOnClickListener { addTaskDialog.dismiss() }
+        updateCloseImg.setOnClickListener { updateTaskDialog.dismiss() }
+
+        mainBinding.addTaskFABtn.setOnClickListener {
+            addTaskDialog.show()
         }
     }
 }
